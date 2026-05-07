d84a55aea0c4320b489ca45ebb9a6b1ae8e6f091
davidzimnitckic <davidzimnitckic@gmail.com>
2026-05-07 11:21:28 +0300

    Add files via upload

---
+++ Зимницкий Давид _17/CinemaApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CinemaApp.Services;
+using CinemaApp.ViewModels;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace CinemaApp
+{
+    public partial class MainWindow : Window
+    {
+        private readonly PipeServerService
+            pipeServer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext =
+                new CinemaViewModel();
+
+            pipeServer =
+                new PipeServerService();
+
+            Loaded += MainWindow_Loaded;
+
+            StartPipeServer();
+        }
+
+        private void MainWindow_Loaded(
+            object sender,
+            RoutedEventArgs e)
+        {
+            Storyboard sb =
+                (Storyboard)Resources[
+                    "FadeInAnimation"
+                ];
+
+            sb.Begin(SeatsPanel);
+        }
+
+        private async void StartPipeServer()
+        {
+            await pipeServer.StartServer();
+        }
+    }
+}
---
+++ Зимницкий Давид _17/CinemaApp/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaApp.Models
+{
+    public class UserModel
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
